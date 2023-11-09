985ab9fd531751eef141b5f44bf498381e18bf6a
MOHAMED ADAN <anwarmoh389@gmail.com>
2023-11-09 00:35:12 +0000

    0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -28,17 +28,17 @@ def stats() -> str:
 
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> None:
-    """ GET /api/v1/unauthorized
+    """GET /api/v1/unauthorized
     Return:
-      - unauthorized error
+      - Unauthorized error.
     """
     abort(401)
 
 
 @app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> None:
-    """ GET /api/v1/Forbidden
+    """GET /api/v1/forbidden
     Return:
-      - Forbidden error
+      - Forbidden error.
     """
     abort(403)
